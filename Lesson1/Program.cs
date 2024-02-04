72557ea961e7692a837f0f52c899ed9f96d2a542
Sinelnikov-Ivan <freecompany2@mail.ru>
2024-02-04 18:10:41 +0700

    all

---
+++ Lesson1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Lesson1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
