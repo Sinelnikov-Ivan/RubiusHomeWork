b565af8eb037a96bbc3481fc0a6e2d79db3fe265
Sinelnikov-Ivan <freecompany2@mail.ru>
2024-02-15 21:00:18 +0700

    Update Cafe.cs

--- Lesson4/Cafe.cs
+++ Lesson4/Cafe.cs
@@ -3,24 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Lesson4
 {
     public class Cafe
     {      
-        public string cafeName;
-        public List<Visitors> ListVisitorsCafe = new List <Visitors>();
+        public string name;
+        public int numberOfAvailableTabless;
+        public List<Visitor> ListVisitorsCafe = new List <Visitor>();
 
-        public Cafe(string _Name) 
+        public Cafe(string _name, int _numberOfAvailableTabless) 
         {
-            cafeName = _Name;  
+            name = _name;
+            numberOfAvailableTabless = _numberOfAvailableTabless;
         }
 
-        public void BookTable (Visitors visitor)  // Бронируем столик  
+        public void BookTable (Visitor visitor)  // Бронируем столик  
         {
-            ListVisitorsCafe.Add (visitor);
-            Console.WriteLine($"В список клиентов кафе {cafeName} добавлен посетитель {visitor.nameVisitor}" );
+            if (numberOfAvailableTabless > 0)
+            {
+            ListVisitorsCafe.Add(visitor);
+            numberOfAvailableTabless--;
+            Console.WriteLine($"В список заказавших столик в кафе {name} добавлен посетитель {visitor.name}");
+            }
+            else
+            {
+            Console.WriteLine($"В кафе {name} нет свободных столиков и посетителю {visitor.name} не смог забронировать столик");
+            }
         }
-
     }
 }
