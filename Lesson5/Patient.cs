35f05649c8918a17698240ae71abd4c6339566db
Sinelnikov-Ivan <freecompany2@mail.ru>
2024-02-18 19:45:57 +0700

    Create Patient.cs

---
+++ Lesson5/Patient.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5
+{
+    public class Patient 
+    {
+        public string name;
+        public int age;
+        public bool disease;
+
+        public Patient (string name, int age, bool disease)
+        {
+            this.name = name;
+            this.age = age;
+            this.disease = disease;
+        }
+
+        public void selfCure ()
+        {
+            Random rnd = new Random();
+            var randomCure = rnd.Next(0, 2);
+
+            if (disease == false)
+            {
+                Console.WriteLine($"{name} начал самолечение:");
+                Console.WriteLine($"но ему лечение не требуется, он здоров!");
+                return;
+            }
+
+            if (randomCure == 1)
+            {
+                Console.WriteLine($"{name} начал самолечение");
+                Console.WriteLine($"и успешно вылечил сам себя");
+                disease = false;                
+            }
+            else
+            {
+                Console.WriteLine($"{name} начал самолечение:");
+                Console.WriteLine($"и не смог вылечить сам себя");                
+            }
+        }
+
+        public void introduceYourself()
+        {   if (disease == false)
+            {
+                Console.WriteLine("Пациент:");
+                Console.WriteLine($"Меня зовут {name}, мне {age} лет и я здоров.");
+                }
+           else {
+                Console.WriteLine("Пациент:");
+                Console.WriteLine($"Меня зовут {name}, мне {age} лет и я болею.");
+            }                       
+        }
+    }
+}
