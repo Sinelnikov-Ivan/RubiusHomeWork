9bf8b0bdf694acc6a0b9b87c352541618a70efb5
Sinelnikov-Ivan <freecompany2@mail.ru>
2024-02-23 00:52:09 +0700

    Create Visitor.cs

---
+++ Lesson4/Visitor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    public class Visitor
+    {
+        public string name;
+        public string favoriteCafe = "NoFavorite";
+        public Visitor(string _name)
+        {
+            name = _name;           
+        }
+
+        public Visitor (string _name, Cafe _favoriteCafe)
+        {
+            name = _name;
+            favoriteCafe = _favoriteCafe.name;
+        }   
+    }
+}
+
+
+
