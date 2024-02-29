a0cec349ccfb2083412d7b7b82908600533b99f5
Sinelnikov-Ivan <freecompany2@mail.ru>
2024-03-01 02:17:02 +0700

    Create Counter.cs

---
+++ Lesson8/Counter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson8
+{
+    public class Counter 
+    {
+        public delegate void myDeligate ();
+        public event myDeligate? myEvent;
+
+         public void startCounter ()          
+        {                     
+            for (int i = 1; i < 100; i++)
+            {
+                Console.WriteLine(i);
+                if (i == 77)
+                {
+                    myEvent.Invoke();
+                }
+            }
+        }
+    }
+}
