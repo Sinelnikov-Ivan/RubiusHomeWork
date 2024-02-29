3e437f12d34bbdbeea3ebeb43be0b58bc7158caa
Sinelnikov-Ivan <freecompany2@mail.ru>
2024-03-01 02:16:53 +0700

    Create Program.cs

---
+++ Lesson8/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Lesson8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {        
+        Handler1 handler1 = new Handler1(); 
+        Handler2 handler2 = new Handler2();
+        Counter counter = new Counter();
+        counter.myEvent += handler1.Say;
+        counter.myEvent += handler2.Say;
+        counter.startCounter();   
+        }
+    }
+}
