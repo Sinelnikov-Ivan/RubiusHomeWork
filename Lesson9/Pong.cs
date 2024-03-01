93cb79bfd92bb304206d58fc740f68e6646c783e
Sinelnikov-Ivan <freecompany2@mail.ru>
2024-03-02 00:48:54 +0700

    Create Pong.cs

---
+++ Lesson9/Pong.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_9
+{
+    internal class Pong
+    {
+        delegate void MyDeligate(); 
+        public static void DoPong()
+        {
+            MyDeligate GoToPing = Ping.DoPing;
+
+            Random rnd = new Random();
+            int value = rnd.Next(0, 100);
+
+            if (value == 0)
+            {
+                Console.WriteLine("Pong промахнулся! Победил Ping");
+            }
+            else
+            {
+                Console.WriteLine("Ping получил Pong");
+                GoToPing();
+            }
+        }
+    }
+}
+
