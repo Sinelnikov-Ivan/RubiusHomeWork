ebc557cd2a6b61a02cf948a806b752a67ad2da83
Sinelnikov-Ivan <freecompany2@mail.ru>
2024-03-02 00:48:52 +0700

    Create Program.cs

---
+++ Lesson9/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Lesson_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ping ping = new Ping();
+            Pong pong = new Pong();
+            Ping.DoPing();
+        }
+    }
+}
